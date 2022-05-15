b4420f85d4fbd5c07636e0739a6093ae71a923c3
AlissonK1 <Alisson.kauan77@gmail.com>
2022-05-15 11:58:33 -0300

    Improving JavaScript

--- script.js
+++ script.js
@@ -8,50 +8,30 @@ const btnSubmit = document.getElementById('btnSubmit');
 const before = document.getElementById('beforeRating');
 const after = document.getElementById('afterRating');
 
-r1.addEventListener('click', () => {
-    r1.classList.toggle('active');
-    r2.classList.remove('active');
-    r3.classList.remove('active');
-    r4.classList.remove('active');
-    r5.classList.remove('active');
-    message.innerText = 'You selected 1 out of 5';
-});
 
-r2.addEventListener('click', () => {
-    r1.classList.remove('active');
-    r2.classList.toggle('active');
-    r3.classList.remove('active');
-    r4.classList.remove('active');
-    r5.classList.remove('active');
-    message.innerText = 'You selected 2 out of 5';
-})
+function getRating(number){
+    let choice = document.getElementById(number);
+    choice.classList.add('active');
+    for(let i = 1; i<=5; i++){
+        let x = ''
+        if(number.includes(String(i))){
+            message.innerText = `You selected ${i} out of 5`;
+            continue;
+        }
+        x = document.getElementById(('r' + i));
+        x.classList.remove('active');
+    }
+}
 
-r3.addEventListener('click', () => {
-    r1.classList.remove('active');
-    r2.classList.remove('active');
-    r3.classList.toggle('active');
-    r4.classList.remove('active');
-    r5.classList.remove('active');
-    message.innerText = 'You selected 3 out of 5';
-})
+r1.addEventListener('click', () => {getRating('r1'); })
 
-r4.addEventListener('click',() => {
-    r1.classList.remove('active');
-    r2.classList.remove('active');
-    r3.classList.remove('active');
-    r4.classList.toggle('active');
-    r5.classList.remove('active');
-    message.innerText = 'You selected 4 out of 5';
-})
+r2.addEventListener('click', () => {getRating('r2'); })
 
-r5.addEventListener('click', () => {
-    r1.classList.remove('active');
-    r2.classList.remove('active');
-    r3.classList.remove('active');
-    r4.classList.remove('active');
-    r5.classList.toggle('active');
-    message.innerText ='You selected 5 out of 5';
-})
+r3.addEventListener('click', () => {getRating('r3'); })
+
+r4.addEventListener('click', () => {getRating('r4'); })
+
+r5.addEventListener('click', () => {getRating('r5'); })
 
 btnSubmit.addEventListener('click', () => {
     before.style.display = 'none';
